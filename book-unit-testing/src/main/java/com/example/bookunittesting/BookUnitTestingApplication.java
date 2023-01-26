f45925866c04e5f2d10b98f47e87a7752360422e
SeungCheol <ssc6839@gmail.com>
2023-01-26 20:57:48 +0900

    [추가] 단위 테스트 초기 설정

---
+++ book-unit-testing/src/main/java/com/example/bookunittesting/BookUnitTestingApplication.java
@@ -0,0 +1,13 @@
+package com.example.bookunittesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookUnitTestingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookUnitTestingApplication.class, args);
+    }
+
+}
---
+++ book-unit-testing/src/test/java/com/example/bookunittesting/BookUnitTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bookunittesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookUnitTestingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
