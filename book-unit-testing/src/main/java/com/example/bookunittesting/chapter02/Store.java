244a657d8c0ecb0e47903ccc9f629b87040af680
SeungCheol <ssc6839@gmail.com>
2023-01-27 23:25:53 +0900

    [수정] 단위 테스트 2장 학습 테스트 수정

--- book-unit-testing/src/main/java/com/example/bookunittesting/chapter02/Store.java
+++ book-unit-testing/src/main/java/com/example/bookunittesting/chapter02/Store.java
@@ -1,31 +1,9 @@
 package com.example.bookunittesting.chapter02;
 
-import java.util.HashMap;
-import java.util.Map;
+public interface Store {
 
-public class Store {
-
-    private final Map<Product, Integer> inventory = new HashMap<>();
-
-    public boolean hasEnoughInventory(final Product product, final int quantity) {
-        return getInventory(product) >= quantity;
-    }
-
-    public void removeInventory(final Product product, final int quantity) {
-        if (!hasEnoughInventory(product, quantity)) {
-            throw new IllegalStateException("Not enough inventory");
-        }
-        inventory.computeIfPresent(product, (key, value) -> value - quantity);
-    }
-
-    public void addInventory(final Product product, final int quantity) {
-        if (inventory.containsKey(product)) {
-            inventory.computeIfPresent(product, (key, value) -> value + quantity);
-        }
-        inventory.put(product, quantity);
-    }
-
-    public int getInventory(final Product product) {
-        return inventory.getOrDefault(product, 0);
-    }
+    boolean hasEnoughInventory(Product product, int quantity);
+    void removeInventory(Product product, int quantity);
+    void addInventory(Product product, int quantity);
+    int getInventory(Product product);
 }
--- book-unit-testing/src/test/java/com/example/bookunittesting/chapter02/CustomerSociableTest.java
+++ book-unit-testing/src/test/java/com/example/bookunittesting/chapter02/CustomerSociableTest.java
@@ -8,25 +8,25 @@ public class CustomerSociableTest {
 
     @Test
     void purchase_succeeds_when_enough_inventory() {
-        final Store store = new Store();
-        store.addInventory(Product.Shampoo, 10);
+        final OnlineStore onlineStore = new OnlineStore();
+        onlineStore.addInventory(Product.Shampoo, 10);
         final Customer sut = new Customer();
 
-        final boolean success = sut.purchase(store, Product.Shampoo, 5);
+        final boolean success = sut.purchase(onlineStore, Product.Shampoo, 5);
 
         assertThat(success).isTrue();
-        assertThat(store.getInventory(Product.Shampoo)).isEqualTo(5);
+        assertThat(onlineStore.getInventory(Product.Shampoo)).isEqualTo(5);
     }
 
     @Test
     void purchase_fails_when_enough_inventory() {
-        final Store store = new Store();
-        store.addInventory(Product.Shampoo, 10);
+        final OnlineStore onlineStore = new OnlineStore();
+        onlineStore.addInventory(Product.Shampoo, 10);
         final Customer sut = new Customer();
 
-        final boolean success = sut.purchase(store, Product.Shampoo, 15);
+        final boolean success = sut.purchase(onlineStore, Product.Shampoo, 15);
 
         assertThat(success).isFalse();
-        assertThat(store.getInventory(Product.Shampoo)).isEqualTo(10);
+        assertThat(onlineStore.getInventory(Product.Shampoo)).isEqualTo(10);
     }
 }
