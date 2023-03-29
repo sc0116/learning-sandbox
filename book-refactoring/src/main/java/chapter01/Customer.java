db25eb58a127ce47bd8a0263450fbd4b51ad38b1
SeungCheol <ssc6839@gmail.com>
2023-03-29 11:34:16 +0900

    refactor: 대여료 계산 메서드를 Rental 클래스로 메서드 이동

--- book-refactoring/src/main/java/chapter01/Customer.java
+++ book-refactoring/src/main/java/chapter01/Customer.java
@@ -18,8 +18,6 @@ public class Customer {
         String result = getName() + " 고객님의 대여 기록\n";
 
         for (final Rental each : rentals) {
-            double thisAmount = amountFor(each);
-
             // 적립 포인트를 1 포인트 증가
             frequentRenterPoints++;
             // 최신물을 이틀 이상 대여하면 보너스 포인트 지급
@@ -31,39 +29,14 @@ public class Customer {
 
             // 이번에 대여한느 비디오 정보와 대여료를 출력
             result += "\t" + each.getMovie().getTitle() + "\t" +
-                    String.valueOf(thisAmount) + "\n";
+                    String.valueOf(each.getCharge()) + "\n";
             // 현재까지 누적된 총 대여료
-            totalAmount += thisAmount;
+            totalAmount += each.getCharge();
             // 푸터 행 추가
         }
 
         result += "누적 대여료: " + String.valueOf(totalAmount) + "\n";
         result += "적립 포인트: " + String.valueOf(frequentRenterPoints);
-        return result;
-    }
-
-    private double amountFor(final Rental rental) {
-        double result = 0;
-
-        // 비디오 종류별 대여료 계산
-        switch (rental.getMovie().getPriceCode()) {
-            case Movie.REGULAR:
-                result += 2;
-                if (rental.getDaysRented() > 2) {
-                    result += (rental.getDaysRented() - 2) * 1.5;
-                }
-                break;
-            case Movie.NEW_RELEASE:
-                result += rental.getDaysRented() * 3;
-                break;
-            case Movie.CHILDRENS:
-                result += 1.5;
-                if (rental.getDaysRented() > 3) {
-                    result += (rental.getDaysRented() - 3) * 1.5;
-                }
-                break;
-        }
-
         return result;
     }
 
--- book-refactoring/src/main/java/chapter01/Rental.java
+++ book-refactoring/src/main/java/chapter01/Rental.java
@@ -10,6 +10,31 @@ public class Rental {
         this.daysRented = daysRented;
     }
 
+    public double getCharge() {
+        double result = 0;
+
+        // 비디오 종류별 대여료 계산
+        switch (getMovie().getPriceCode()) {
+            case Movie.REGULAR:
+                result += 2;
+                if (getDaysRented() > 2) {
+                    result += (getDaysRented() - 2) * 1.5;
+                }
+                break;
+            case Movie.NEW_RELEASE:
+                result += getDaysRented() * 3;
+                break;
+            case Movie.CHILDRENS:
+                result += 1.5;
+                if (getDaysRented() > 3) {
+                    result += (getDaysRented() - 3) * 1.5;
+                }
+                break;
+        }
+
+        return result;
+    }
+
     public Movie getMovie() {
         return movie;
     }
