4332b5cab55ad939924d1e6540fbbf984a275c07
SeungCheol <ssc6839@gmail.com>
2023-03-29 12:19:22 +0900

    refactor: 임시변수를 메서드 호출로 전환

--- book-refactoring/src/main/java/chapter01/Customer.java
+++ book-refactoring/src/main/java/chapter01/Customer.java
@@ -13,23 +13,32 @@ public class Customer {
     }
 
     public String statement() {
-        double totalAmount = 0;
-        int frequentRenterPoints = 0;
         String result = getName() + " 고객님의 대여 기록\n";
-
         for (final Rental each : rentals) {
-            frequentRenterPoints += each.getFrequentRenterPoints();
-
-            // 이번에 대여한느 비디오 정보와 대여료를 출력
+            // 이번에 대여하는 비디오 정보와 대여료를 출력
             result += "\t" + each.getMovie().getTitle() + "\t" +
                     String.valueOf(each.getCharge()) + "\n";
-            // 현재까지 누적된 총 대여료
-            totalAmount += each.getCharge();
-            // 푸터 행 추가
         }
 
-        result += "누적 대여료: " + String.valueOf(totalAmount) + "\n";
-        result += "적립 포인트: " + String.valueOf(frequentRenterPoints);
+        // 푸터 행 추가
+        result += "누적 대여료: " + String.valueOf(getTotalCharge()) + "\n";
+        result += "적립 포인트: " + String.valueOf(getTotalFrequentRenterPoints());
+        return result;
+    }
+
+    private double getTotalCharge() {
+        double result = 0;
+        for (final Rental rental : rentals) {
+            result += rental.getCharge();
+        }
+        return result;
+    }
+
+    public int getTotalFrequentRenterPoints() {
+        int result = 0;
+        for (final Rental rental : rentals) {
+            result += rental.getFrequentRenterPoints();
+        }
         return result;
     }
 
