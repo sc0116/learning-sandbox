1f2fd1665034fd64345ba29aed1e9428ff078b33
SeungCheol <ssc6839@gmail.com>
2023-05-29 14:55:10 +0900

    feat: TransactionalEventListener 적용

--- spring-events/src/main/java/learning/springevents/order/infrastructure/OrderCanceledEventHandler.java
+++ spring-events/src/main/java/learning/springevents/order/infrastructure/OrderCanceledEventHandler.java
@@ -3,9 +3,10 @@ package learning.springevents.order.infrastructure;
 import learning.springevents.order.application.RefundService;
 import learning.springevents.order.domain.OrderCanceledEvent;
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.event.TransactionPhase;
+import org.springframework.transaction.event.TransactionalEventListener;
 
 @RequiredArgsConstructor
 @Component
@@ -14,7 +15,10 @@ public class OrderCanceledEventHandler {
     private final RefundService refundService;
 
     @Async
-    @EventListener(OrderCanceledEvent.class)
+    @TransactionalEventListener(
+            value = OrderCanceledEvent.class,
+            phase = TransactionPhase.AFTER_COMMIT
+    )
     public void handle(final OrderCanceledEvent event) {
         refundService.refund(event.getOrderNumber());
     }
