ccbfdac0beae9eb1254684f03aee31464c22db87
SeungCheol <ssc6839@gmail.com>
2023-05-30 14:34:28 +0900

    feat: synchronized 키워드 사용 전 코드 및 테스트 작성

---
+++ spring-concurrency/synchronized/src/main/java/learning/_synchronized/domain/Product.java
@@ -0,0 +1,35 @@
+package learning._synchronized.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private Long quantity;
+
+    public Product(final String name, final Long quantity) {
+        this.name = name;
+        this.quantity = quantity;
+    }
+
+    public void decrease(final Long quantity) {
+        if (getQuantity() < quantity) {
+            throw new RuntimeException("수량이 부족합니다.");
+        }
+        this.quantity -= quantity;
+    }
+}
