e92222176bd4ec6e82e9bfc04328a97284f90313
SeungCheol <ssc6839@gmail.com>
2023-05-30 12:25:02 +0900

    feat: 프로젝트 초기 설정

---
+++ spring-concurrency/synchronized/src/main/java/learning/_synchronized/SynchronizedApplication.java
@@ -0,0 +1,13 @@
+package learning._synchronized;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SynchronizedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SynchronizedApplication.class, args);
+    }
+
+}
---
+++ spring-concurrency/synchronized/src/test/java/learning/_synchronized/SynchronizedApplicationTests.java
@@ -0,0 +1,13 @@
+package learning._synchronized;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SynchronizedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
