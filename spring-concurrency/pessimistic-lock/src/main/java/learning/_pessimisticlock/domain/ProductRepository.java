670c107e8033ed2290a9c19b52dad797fbeb4581
SeungCheol <ssc6839@gmail.com>
2023-05-31 21:33:45 +0900

    feat: 비관적 락 적용 전 코드 및 테스트 작성

---
+++ spring-concurrency/pessimistic-lock/src/main/java/learning/_pessimisticlock/domain/ProductRepository.java
@@ -0,0 +1,9 @@
+package learning._pessimisticlock.domain;
+
+import java.util.Optional;
+import org.springframework.data.repository.Repository;
+
+public interface ProductRepository extends Repository<Product, Long> {
+    Product save(Product entity);
+    Optional<Product> findById(Long id);
+}
