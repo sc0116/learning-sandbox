8975c5f39e5b3aaac51198093e525043621b2296
SeungCheol <ssc6839@gmail.com>
2023-05-31 17:21:09 +0900

    feat: 프로젝트 초기 설정

---
+++ spring-concurrency/pessimistic-lock/src/test/java/learning/_pessimisticlock/PessimisticLockApplicationTests.java
@@ -0,0 +1,13 @@
+package learning._pessimisticlock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PessimisticLockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
