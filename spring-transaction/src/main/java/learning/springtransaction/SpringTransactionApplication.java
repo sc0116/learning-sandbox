74f4a2e178678e0f101caef0be0c75924ea9438e
SeungCheol <ssc6839@gmail.com>
2023-06-08 23:27:51 +0900

    feat: 프로젝트 초기 설정

---
+++ spring-transaction/src/main/java/learning/springtransaction/SpringTransactionApplication.java
@@ -0,0 +1,13 @@
+package learning.springtransaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTransactionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTransactionApplication.class, args);
+    }
+
+}
---
+++ spring-transaction/src/test/java/learning/springtransaction/SpringTransactionApplicationTests.java
@@ -0,0 +1,13 @@
+package learning.springtransaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTransactionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
