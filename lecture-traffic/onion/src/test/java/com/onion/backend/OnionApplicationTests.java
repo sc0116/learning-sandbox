f3380aa90e63a5cd9a1bc01f43ff2d8c42d4c493
jjanggu <ssc6839@gmail.com>
2024-12-10 01:14:34 +0900

    feat: 개발환경 세팅

---
+++ lecture-traffic/onion/src/test/java/com/onion/backend/OnionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onion.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
