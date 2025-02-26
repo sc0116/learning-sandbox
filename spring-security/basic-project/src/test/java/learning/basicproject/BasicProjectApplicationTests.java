7a07c68c39d690a4871cce7f929018261ba4dd31
jjanggu <ssc6839@gmail.com>
2025-02-26 11:36:05 +0900

    feat: 실습 프로젝트 초기 설정

---
+++ spring-security/basic-project/src/test/java/learning/basicproject/BasicProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package learning.basicproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
